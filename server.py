9843d4523c0c3d56af1741616f5bb80a84e64a3c
aqil <datapirates69@gmail.com>
2023-05-16 12:35:16 +0800

    clean up phase 2

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    # return "<p>Hello, World! aqil</p>"
     return render_template("index.html")
-    
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
